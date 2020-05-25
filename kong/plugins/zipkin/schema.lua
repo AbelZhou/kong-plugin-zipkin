f91b25508ed911f8d68cb4bf1c4c507767247e3e
Enrique García Cota <kikito@gmail.com>
2020-05-25 11:54:36 +0200

    feat(handler) static tags
    Allows setting tags on the request span via configuration.
    
    Implements #3

--- kong/plugins/zipkin/schema.lua
+++ kong/plugins/zipkin/schema.lua
@@ -1,4 +1,29 @@
 local typedefs = require "kong.db.schema.typedefs"
+local Schema = require "kong.db.schema"
+
+local PROTECTED_TAGS = {
+  "error",
+  "http.method",
+  "http.path",
+  "http.status_code",
+  "kong.balancer.state",
+  "kong.balancer.try",
+  "kong.consumer",
+  "kong.credential",
+  "kong.node.id",
+  "kong.route",
+  "kong.service",
+  "lc",
+  "peer.hostname",
+}
+
+local static_tag = Schema.define {
+  type = "record",
+  fields = {
+    { name = { type = "string", required = true, not_one_of = PROTECTED_TAGS } },
+    { value = { type = "string", required = true } },
+  },
+}
 
 return {
   name = "zipkin",
@@ -15,6 +40,7 @@ return {
           { traceid_byte_count = { type = "integer", required = true, default = 16, one_of = { 8, 16 } } },
           { header_type = { type = "string", required = true, default = "preserve",
                             one_of = { "preserve", "b3", "b3-single", "w3c" } } },
+          { static_tags = { type = "array", elements = static_tag } }
         },
     }, },
   },
